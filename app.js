45b95dd6d4a8d4b6f35e400e077eba0318ebaa70
Betinho1990 <robertofilho.apoena@gmai.com>
2025-03-02 19:07:36 -0300

    7daysOfCode-3dia

---
+++ app.js
@@ -0,0 +1,62 @@
+document.getElementById("area").addEventListener("change", function() {
+    let area = this.value;
+    let tecnologiaSelect = document.getElementById("tecnologia");
+    let labelTecnologia = document.getElementById("labelTecnologia");
+    tecnologiaSelect.innerHTML = "";
+
+    while (tecnologiaSelect.options.length > 0) {
+        tecnologiaSelect.remove(0);
+    }
+
+    if (area === "front-end") {
+        labelTecnologia.innerText = "Você quer aprender React ou Vue?";
+        let options = ["React", "Vue"];
+        options.forEach(option => {
+            let newOption = document.createElement("option");
+            newOption.value = option;
+            newOption.textContent = option;
+            tecnologiaSelect.appendChild(newOption);
+        });
+        labelTecnologia.style.display = "block";
+        tecnologiaSelect.style.display = "block";
+    } else if (area === "back-end") {
+        labelTecnologia.innerText = "Você quer aprender C# ou Java?";
+        let options = ["C#", "Java"];
+        options.forEach(option => {
+            let newOption = document.createElement("option");
+            newOption.value = option;
+            newOption.textContent = option;
+            tecnologiaSelect.appendChild(newOption);
+        });
+        labelTecnologia.style.display = "block";
+        tecnologiaSelect.style.display = "block";
+    }
+});
+
+function continuarJogo() {
+    let tecnologia = document.getElementById("tecnologia").value;
+    let caminho = document.getElementById("caminho").value;
+    
+    if (caminho === "especializar") {
+        alert(`Ótima escolha! Seguir se especializando em ${tecnologia} vai te tornar um expert.`);
+    } else {
+        alert("Expandir seus conhecimentos para se tornar Fullstack é um grande passo!");
+    }
+
+    let divTecnologias = document.getElementById("tecnologiasExtras");
+    divTecnologias.innerHTML = "<label>Digite outras tecnologias que deseja aprender:</label>";
+    divTecnologias.innerHTML += "<input type='text' id='novaTecnologia'><button onclick='adicionarTecnologia()'>Adicionar</button>";
+    divTecnologias.innerHTML += "<ul id='listaTecnologias'></ul>";
+}
+
+function adicionarTecnologia() {
+    let inputNovaTecnologia = document.getElementById("novaTecnologia");
+    let lista = document.getElementById("listaTecnologias");
+    
+    if (inputNovaTecnologia.value.trim() !== "") {
+        let li = document.createElement("li");
+        li.textContent = inputNovaTecnologia.value;
+        lista.appendChild(li);
+        inputNovaTecnologia.value = "";
+    }
+}
